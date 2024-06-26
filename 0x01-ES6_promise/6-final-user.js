647dde59395f2da102d902be6b617d0bc7d349cc
elsaaeid <saidsadaoy@gmail.com>
2024-06-26 19:55:41 +0000

    deploy changes

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,25 +1,14 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ];
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-  return Promise.allSettled(promises).then((results) => {
-    return results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return {
-          status: result.status,
-          value: result.value,
-        };
-      } else {
-        return {
-          status: result.status,
-          value: result.reason.message,
-        };
-      }
-    });
-  });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : String(result.reason),
+      }))
+    ));
 }
