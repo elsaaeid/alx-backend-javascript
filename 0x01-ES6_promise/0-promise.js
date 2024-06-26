cff944768ff4fa90a7122e3eeacc5ccfbc7b467b
elsaaeid <saidsadaoy@gmail.com>
2024-06-26 20:15:14 +0000

    deploy changes

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,9 @@
-export default function getResponseFromAPI() {
-  return new Promise(() => {});
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Perform API request or any asynchronous operation here
+    // Once the operation is complete, call resolve() with the result
+    // If there's an error, call reject() with the error
+  });
 }
+
+export default getResponseFromAPI;
