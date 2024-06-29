e591a593f037e7e68210051ec5d37d6ecf26164f
elsaaeid <saidsadaoy@gmail.com>
2024-06-29 00:47:35 +0000

    deploy changes

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,41 +1,26 @@
-class Currency {
-    constructor(code, name) {
-      if (typeof code !== 'string') {
-        throw new TypeError('Code must be a string');
-      }
-      if (typeof name !== 'string') {
-        throw new TypeError('Name must be a string');
-      }
-      this._code = code;
-      this._name = name;
-    }
-  
-    get code() {
-      return this._code;
-    }
-  
-    set code(newCode) {
-      if (typeof newCode !== 'string') {
-        throw new TypeError('Code must be a string');
-      }
-      this._code = newCode;
-    }
-  
-    get name() {
-      return this._name;
-    }
-  
-    set name(newName) {
-      if (typeof newName !== 'string') {
-        throw new TypeError('Name must be a string');
-      }
-      this._name = newName;
-    }
-  
-    displayFullCurrency() {
-      return `${this._name} (${this._code})`;
-    }
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
   }
-  
-  const dollar = new Currency('$', 'Dollars');
-  console.log(dollar.displayFullCurrency());
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
