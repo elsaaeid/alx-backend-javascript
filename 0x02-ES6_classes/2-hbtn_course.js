23b96813939ee4b041cb589c853047d094fbd067
elsaaeid <saidsadaoy@gmail.com>
2024-06-29 00:41:07 +0000

    deploy changes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,60 +1,43 @@
-class HolbertonCourse {
+export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
-  
+
   get name() {
     return this._name;
   }
-  
-  set name(newName) {
-    if (typeof newName !== 'string') {
+
+  set name(value) {
+    if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
     }
-    this._name = newName;
+    this._name = value;
   }
-  
+
   get length() {
     return this._length;
   }
-  
-  set length(newLength) {
-    if (typeof newLength !== 'number') {
+
+  set length(value) {
+    if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    this._length = newLength;
+    this._length = value;
   }
-  
+
   get students() {
     return this._students;
   }
-  
-  set students(newStudents) {
-    this._students = newStudents;
+
+  set students(value) {
+    if (!(value instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = value;
   }
 }
-  
-  const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"]);
-  console.log(c1.name);
-  c1.name = "Python 101";
-  console.log(c1);
-  
-  try {
-    c1.name = 12;
-  } catch (err) {
-    console.log(err);
-  }
-  
-  try {
-    const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]);
-  } catch (err) {
-    console.log(err);
-  }
