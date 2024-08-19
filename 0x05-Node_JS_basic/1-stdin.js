0110097557391bd683b913bb6a2cebe0ea8aa57b
elsaaeid <saidsadaoy@gmail.com>
2024-08-19 23:11:28 +0000

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,5 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+const msg1 = 'Welcome to Holberton School, what is your name?\n';
+process.stdout.write(msg1);
 
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
@@ -11,6 +12,7 @@ if (process.stdin.isTTY) {
     process.exit();
   });
   process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
+    const msg2 = 'This important software is now closing\n';
+    process.stdout.write(msg2);
   });
 }
