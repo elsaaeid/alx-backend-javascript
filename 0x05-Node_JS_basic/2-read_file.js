056ea86e856ddcd871b4d369522e241a67ab728d
elsaaeid <saidsadaoy@gmail.com>
2024-08-20 19:04:35 +0000

    update

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
+/**
+ * Counts the number of students in each field from a CSV file.
+ * @param {string} filePath - Path to the database file.
+ */
 
-
-function countStudents(path) {
+function countStudents(filePath) {
   try {
-    const lines = fs.readFileSync(path, { encoding: 'utf8' }).split(/\r?\n/);
+    const results = fs.readFileSync(filePath, { encoding: 'utf8' }).split(/\r?\n/);
+    const lines = results;
     let i = 0;
     let countStudents = 0;
     const fields = {};
@@ -11,15 +15,15 @@ function countStudents(path) {
     for (const line of lines) {
       if (line.trim() !== '' && i > 0) {
         countStudents += 1;
-        const [fname, lname, age, field] = line.split(','); // eslint-disable-line
+        const [field, data] = line.split(',');
         if (!fields[field]) {
           fields[field] = {
             count: 1,
-            students: [fname],
+            students: [data.fname],
           };
         } else {
           const newCount = fields[field].count + 1;
-          const newStudents = (fields[field].students).concat(fname);
+          const newStudents = (fields[field].students).concat(data.fname);
           fields[field] = {
             count: newCount,
             students: newStudents,
@@ -31,9 +35,9 @@ function countStudents(path) {
 
     console.log(`Number of students: ${countStudents}`);
     for (const field of Object.keys(fields)) {
-      const n = fields[field].count;
+      const studentsNum = fields[field].count;
       const names = fields[field].students.join(', ');
-      console.log(`Number of students in ${field}: ${n}. List: ${names}`);
+      console.log(`Number of students in ${field}: ${studentsNum}. List: ${names}`);
     }
   } catch (error) {
     throw new Error('Cannot load the database');
