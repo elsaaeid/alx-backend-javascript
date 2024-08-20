158d793b278c17cca4eb748eb70f457f8f9202ca
elsaaeid <saidsadaoy@gmail.com>
2024-08-20 18:51:41 +0000

    update

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const results = fs.readFileSync(path, { encoding: 'utf8' }).split(/\r?\n/);
-    const lines = results;
+    const lines = fs.readFileSync(path, { encoding: 'utf8' }).split(/\r?\n/);
     let i = 0;
     let countStudents = 0;
     const fields = {};
