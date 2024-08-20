a8e20f11747ece4a74f0b781a965813adc3954bf
elsaaeid <saidsadaoy@gmail.com>
2024-08-20 16:51:02 +0000

    update

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,22 +1,22 @@
 import fs from 'fs/promises';
 
 export const readDatabase = async (filePath) => {
-    try {
-        const data = await fs.readFile(filePath, 'utf8');
-        const lines = data.split('\n').filter(line => line.trim() !== '');
-        const students = {};
+  try {
+    const data = await fs.readFile(filePath, 'utf8');
+    const lines = data.split('\n').filter(line => line.trim() !== '');
+    const students = {};
 
-        lines.forEach((line, index) => {
-            if (index === 0) return; // Skip header
-            const [firstName, field] = line.split(',');
-            if (!students[field]) {
-                students[field] = [];
-            }
-            students[field].push(firstName);
-        });
+    lines.forEach((line, index) => {
+      if (index === 0) return; // Skip header
+      const [firstName, field] = line.split(',');
+      if (!students[field]) {
+        students[field] = [];
+      }
+      students[field].push(firstName);
+    });
 
-        return students;
-    } catch (error) {
-        throw error; // Reject the promise with the error
-    }
+    return students;
+  } catch (error) {
+    throw error; // Reject the promise with the error
+  }
 };
