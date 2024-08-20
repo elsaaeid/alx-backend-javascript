4b06350b58f9d5f427d485ce53ab18d6348f7079
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-08-20 13:59:57 -0700

    Update AppController.js

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,5 +1,7 @@
-export class AppController {
-  static getHomepage(req, res) {
-    res.status(200).send('Hello Holberton School!\n');
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
   }
 }
+
+module.exports = AppController;
