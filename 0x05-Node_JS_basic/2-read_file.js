07f76406b2b803c21bf181e4b1fa3b9f465fddf4
elsaaeid <saidsadaoy@gmail.com>
2024-08-20 19:29:50 +0000

    update

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,43 +1,46 @@
 const fs = require('fs');
 /**
  * Counts the number of students in each field from a CSV file.
- * @param {string} filePath - Path to the database file.
+ * @param {string} path - Path to the database file.
  */
 
-function countStudents(filePath) {
+
+function countStudents(path) {
   try {
-    const results = fs.readFileSync(filePath, { encoding: 'utf8' }).split(/\r?\n/);
+    const results = fs.readFileSync(path, { encoding: 'utf8' }).split(/\r?\n/);
     const lines = results;
     let i = 0;
     let countStudents = 0;
     const fields = {};
 
-    for (const line of lines) {
+    // Use a while loop to iterate through the lines
+    while (i < lines.length) {
+      const line = lines[i];
       if (line.trim() !== '' && i > 0) {
         countStudents += 1;
-        const [field, data] = line.split(',');
+        const [fname, lname, age, field] = line.split(','); // eslint-disable-line
         if (!fields[field]) {
           fields[field] = {
             count: 1,
-            students: [data.fname],
+            students: [fname],
           };
         } else {
           const newCount = fields[field].count + 1;
-          const newStudents = (fields[field].students).concat(data.fname);
+          const newStudents = fields[field].students.concat(fname);
           fields[field] = {
             count: newCount,
             students: newStudents,
           };
         }
       }
-      i += 1;
+      i += 1; // Increment the index
     }
 
     console.log(`Number of students: ${countStudents}`);
     for (const field of Object.keys(fields)) {
-      const studentsNum = fields[field].count;
+      const n = fields[field].count;
       const names = fields[field].students.join(', ');
-      console.log(`Number of students in ${field}: ${studentsNum}. List: ${names}`);
+      console.log(`Number of students in ${field}: ${n}. List: ${names}`);
     }
   } catch (error) {
     throw new Error('Cannot load the database');
