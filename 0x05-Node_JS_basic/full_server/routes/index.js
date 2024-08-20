6bf86091e9220e898c353d197f5136e3b7ec9bca
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-08-20 15:46:26 -0700

    Update index.js

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const AppController = require('../controllers/AppController');
-const StudentsController = require('../controllers/StudentsController');
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
 
-const app = express();
 
-app.get('/', ((request, response) => AppController.getHomepage(request, response)));
-app.get('/students', ((request, response) => StudentsController.getAllStudents(request, response)));
-app.get('/students/:major', ((request, response) => StudentsController.getAllStudentsByMajor(request, response)));
+const mapRoutes = (app) => {
+  app.get('/', AppController.getHomepage);
+  app.get('/students', StudentsController.getAllStudents);
+  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+};
 
-module.exports = app;
+export default mapRoutes;
+module.exports = mapRoutes;
