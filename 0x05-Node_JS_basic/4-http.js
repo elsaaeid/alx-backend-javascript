f2b899f892295df497eb08756d9d3fc2c9cdf137
elsaaeid <saidsadaoy@gmail.com>
2024-08-20 16:03:34 +0000

    update

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -6,10 +6,8 @@ const http = require('http');
 
 // Create an HTTP server
 const app = http.createServer((req, res) => {
-  // Set the response HTTP header with HTTP status and Content type
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  // Send the response body
-  res.end('Hello Holberton School!\n');
+  res.write('Hello Holberton School!');
+  res.end();
 });
 
 // Make the server listen on port 1245
