09920e09d4cef3c92d1b76323909939e640811e9
elsaaeid <saidsadaoy@gmail.com>
2024-08-20 18:58:08 +0000

    update

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -10,7 +10,7 @@ function countStudents(path) {
     for (const line of lines) {
       if (line.trim() !== '' && i > 0) {
         countStudents += 1;
-        const [field, fname, lname, age] = line.split(','); // eslint-disable-line
+        const [fname, lname, age, field] = line.split(','); // eslint-disable-line
         if (!fields[field]) {
           fields[field] = {
             count: 1,
