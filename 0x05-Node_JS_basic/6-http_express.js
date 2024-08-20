06906ab174e516606f98443c931b7ef152a54c6d
elsaaeid <saidsadaoy@gmail.com>
2024-08-20 16:19:47 +0000

    update

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,20 +1,15 @@
 /**
  * Create a small HTTP server using Express
  */
-
 const express = require('express');
 
 const app = express();
 const PORT = 1245;
 
-
-// Define the root endpoint
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-// Make the server listen on port 1245
 app.listen(PORT, () => console.log(`Listening on port ${PORT}\n`));
 
-// Export the app
 module.exports = app;
