a180267fed1b297b7bba5e57ea12adc644d5f07c
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-08-20 13:59:34 -0700

    Update utils.js

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,22 +1,40 @@
-import fs from 'fs';
+const fs = require('fs');
 
-export const readDatabase = async (filePath) => {
-  try {
-    const data = await fs.readFile(filePath, 'utf8');
-    const lines = data.split('\n').filter(line => line.trim() !== '');
-    const students = {};
+const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, (error, csvData) => {
+    if (error) {
+      reject(Error('Cannot load the database'));
+    }
+    if (csvData) {
+      const fields = {};
+      const dataShow = {};
+      let data = csvData.toString().split('\n');
+      data = data.filter((element) => element.length > 0);
 
-    lines.forEach((line, index) => {
-      if (index === 0) return; // Skip header
-      const [firstName, field] = line.split(',');
-      if (!students[field]) {
-        students[field] = [];
+      data.shift();
+      data.forEach((element) => {
+        if (element.length > 0) {
+          const row = element.split(',');
+          if (row[3] in fields) {
+            fields[row[3]].push(row[0]);
+          } else {
+            fields[row[3]] = [row[0]];
+          }
+        }
+      });
+      for (const field in fields) {
+        if (field) {
+          const list = fields[field];
+          dataShow[field] = {
+            list: `List: ${list.toString().replace(/,/g, ', ')}`,
+            number: list.length,
+          };
+        }
       }
-      students[field].push(firstName);
-    });
 
-    return students;
-  } catch (error) {
-    throw error; // Reject the promise with the error
-  }
-};
+      resolve(dataShow);
+    }
+  });
+});
+
+module.exports = readDatabase;
