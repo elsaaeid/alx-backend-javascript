cef8189fc81b42e438a333ea43910faa47d0002c
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-08-21 09:52:37 -0700

    Update StudentsController.js

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,29 +1,39 @@
 const readDatabase = require('../utils');
 
-class StudentsController {
-  static getAllStudents(req, res) {
-    const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
-    readDatabase(DB_FILE)
-      .then((studentGroups) => {
-        const printData = [];
-        printData.push('This is the list of our students');
-        for (const field in studentGroups) {
-          if (field) printData.push(`Number of students in ${field}: ${studentGroups[field].number}. ${studentGroups[field].list}`);
-        }
-        res.send(printData.join('\n'));
-      })
-      .catch((err) => { res.send(err.message); });
+const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
+export class StudentsController {
+  static async getAllStudents(req, res) {
+    try {
+      const studentGroups = await readDatabase(DB_FILE);
+      const reports = ['This is the list of our students'];
+
+      Object.keys(studentGroups)
+        .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase())) // Sort fields case-insensitively
+        .forEach((field) => {
+          reports.push(`Number of students in ${field}: ${studentGroups[field].length}. List: ${studentGroups[field].map(student => student.firstname).join(', ')}`);
+        });
+
+      res.status(200).send(reports.join('\n'));
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
   }
 
-  static getAllStudentsByMajor(req, res) {
-    if (!['SWE', 'CS'].includes(req.params.major)) res.status(500).send('Major parameter must be CS or SWE');
-    else {
-      readDatabase(process.argv[2])
-        .then((studentGroups) => {
-          if (Object.keys(studentGroups).length > 0) res.send(studentGroups[req.params.major].list);
-          res.send(500, 'Cannot load the database');
-        })
-        .catch((err) => { res.send(err.message); });
+  static async getAllStudentsByMajor(req, res) {
+    const major = req.params.major;
+
+    if (!['CS', 'SWE'].includes(major)) {
+      return res.status(500).send('Major parameter must be CS or SWE');
+    }
+
+    try {
+      const studentGroups = await readDatabase(DB_FILE);
+      const studentsInMajor = studentGroups[major] || [];
+      const firstNames = studentsInMajor.map(student => student.firstname).join(', ');
+
+      res.status(200).send(`List: ${firstNames}`);
+    } catch (error) {
+      res.status(500).send(error.message);
     }
   }
 }
