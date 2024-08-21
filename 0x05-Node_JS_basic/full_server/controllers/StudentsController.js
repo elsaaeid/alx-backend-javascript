75e6d1bf3620e2d08e2d3ddc9776235cc8226c07
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-08-21 09:29:45 -0700

    Update StudentsController.js

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -4,11 +4,11 @@ class StudentsController {
   static getAllStudents(req, res) {
     const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
     readDatabase(DB_FILE)
-      .then((data) => {
+      .then((studentGroups) => {
         const printData = [];
         printData.push('This is the list of our students');
-        for (const field in data) {
-          if (field) printData.push(`Number of students in ${field}: ${data[field].number}. ${data[field].list}`);
+        for (const field in studentGroups) {
+          if (field) printData.push(`Number of students in ${field}: ${studentGroups[field].number}. ${studentGroups[field].list}`);
         }
         res.send(printData.join('\n'));
       })
@@ -19,8 +19,8 @@ class StudentsController {
     if (!['SWE', 'CS'].includes(req.params.major)) res.status(500).send('Major parameter must be CS or SWE');
     else {
       readDatabase(process.argv[2])
-        .then((data) => {
-          if (Object.keys(data).length > 0) res.send(data[req.params.major].list);
+        .then((studentGroups) => {
+          if (Object.keys(studentGroups).length > 0) res.send(studentGroups[req.params.major].list);
           res.send(500, 'Cannot load the database');
         })
         .catch((err) => { res.send(err.message); });
