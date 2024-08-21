c81e67531017d1ea67ee7589f16a2e83ef556339
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-08-21 06:05:47 -0700

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -8,15 +8,13 @@ const countStudents = require('./3-read_file_async');
 
 // Port
 const PORT = 1245;
-// Get file if argument was passed
+// Get file if arguement was passed
 const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
-
 // HTTP SERVER
 const server = http.createServer((req, res) => {
   const { pathname } = url.parse(req.url, true);
 
   if (pathname === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Hello Holberton School!');
     res.end();
   } else if (pathname === '/students') {
@@ -25,26 +23,15 @@ const server = http.createServer((req, res) => {
 
     countStudents(DB_FILE)
       .then((data) => {
-        // Assuming data is an object with student counts and lists
-        const { totalStudents, csStudents, sweStudents } = data;
-
-        studentReport.push(`Number of students: ${totalStudents}`);
-        studentReport.push(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
-        studentReport.push(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
-
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        studentReport.push(data);
         res.write(studentReport.join('\n'));
         res.end();
       })
       .catch((err) => {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.write('Error reading student data: ' + (err instanceof Error ? err.message : err.toString()));
+        studentReport.push(err instanceof Error ? err.message : err.toString());
+        res.write(studentReport.join('\n'));
         res.end();
       });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.write('Not Found');
-    res.end();
   }
 });
 
@@ -53,5 +40,4 @@ const app = server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
 
-// Export the app
 module.exports = app;
