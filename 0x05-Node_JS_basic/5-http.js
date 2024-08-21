d92399e5763d5083057966fe01720ad88d1f3743
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-08-21 08:00:58 -0700

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -5,8 +5,6 @@ const url = require('url');
 const PORT = 1245;
 const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
 
-const fs = require('fs').promises;
-
 const countStudents = (dataPath) => new Promise((resolve, reject) => {
   if (!dataPath) {
     reject(new Error('Cannot load the database'));
