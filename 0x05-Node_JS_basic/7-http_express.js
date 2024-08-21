1723b09a93a88f25ac722a264fd4c48518eb97b6
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-08-21 09:01:25 -0700

    Update 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -2,13 +2,61 @@
  * Create a more complex HTTP server using Express
  */
 const express = require('express');
-// Import the countStudents function
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const app = express();
 const PORT = 1245;
 // Get file if arguement was passed
 const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
+const countStudents = (dataPath) => new Promise((resolve, reject) => {
+  if (!dataPath) {
+    reject(new Error('Cannot load the database'));
+    return; // Ensure we exit the function after rejection
+  }
+  fs.readFile(dataPath, (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return; // Ensure we exit the function after rejection
+    }
+
+    const reports = [];
+    const fileLines = data.toString('utf-8').trim().split('\n');
+    const studentGroups = {};
+    const FieldNames = fileLines[0].split(',');
+    const studentPropertyNames = FieldNames.slice(0, FieldNames.length - 1);
+    let index = 1; // Start from the second line (after header)
+    while (index < fileLines.length) {
+      const line = fileLines[index];
+      const studentRecord = line.split(',');
+      const studentPropertyValues = studentRecord.slice(0, studentRecord.length - 1);
+      const field = studentRecord[studentRecord.length - 1];
+
+      if (!Object.keys(studentGroups).includes(field)) {
+        studentGroups[field] = [];
+      }
+
+      const studentEntries = studentPropertyNames.map((propertyName, idx) => [
+        propertyName,
+        studentPropertyValues[idx],
+      ]);
+      studentGroups[field].push(Object.fromEntries(studentEntries));
+      index += 1;
+    }
+    const totalStudents = Object.values(studentGroups).reduce(
+      (pre, cur) => (pre || []).length + cur.length,
+    );
+    reports.push(`Number of students: ${totalStudents}`);
+    for (const [field, group] of Object.entries(studentGroups)) {
+      reports.push([
+        `Number of students in ${field}: ${group.length}.`,
+        'List:',
+        group.map((student) => student.firstname).join(', '),
+      ].join(' '));
+    }
+    resolve(reports.join('\n'));
+  });
+});
+
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
